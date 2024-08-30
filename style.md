9b7a7e7af414a2dc54ee893bd09865eb2d94fc26
Janpol Hidalgo <poyhidalgo@gmail.com>
2024-08-30 19:38:17 +0800

    Finished the login page. I'll soon make some design changes to the index page. Next up is the registration page.

---
+++ style.md
@@ -0,0 +1,13 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+
+// <uniquifier>: Use a unique and descriptive class name
+// <weight>: Use a value from 300 to 900
+
+.figtree-<uniquifier> {
+  font-family: "Figtree", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
+}
--- views/register.php
+++ views/register.php
@@ -8,23 +8,10 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="../public/styles/styles.css">
+        <link rel="stylesheet" href="../public/styles/">
     </head>
     <body>
-        <div class="container">
-            <div class="box">
-                <img src="../public/images/wave-cat.png" alt="waving cat" width="200">
-                <h2>Welcome Back!</h2>
-                <p>Don't have an account yet? <a href="./register.php">Sign up</a></p>
-                <form action="post">
-                    <input type="email" name="email" autocomplete="off" placeholder="Email">
-                    <input type="password" name="password" autocomplete="new-password" placeholder="Password">
-                    <button type="submit">Sign in</button>
-                </form>
-                <span>Forgot your password?</span>
-            </div>
-        </div>
-
+        
         <script src=""></script>
     </body>
 </html>
